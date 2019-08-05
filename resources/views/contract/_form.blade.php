985bb1a81fce99b7f49094c8cd21bf6b3ead0934
HaPham <phammanhha305@gmail.com>
2019-08-06 06:18:22 +0700

    06/08/2019

--- resources/views/contract/_form.blade.php
+++ resources/views/contract/_form.blade.php
@@ -230,9 +230,9 @@
             //Add or remove row to table
             $('.box-footer').on('click', '.addRow:not(".disabled")',function (e) {
                 e.preventDefault();
-                let tableBody = $('tbody');
+                let tableBody = $('#example1 tbody');
                 let numberOfProduct = tableBody.children().length;
-                let lastRow = $('tr:last');
+                let lastRow = tableBody.find('tr:last');
                 let newRow = lastRow.clone();
                 let select2 = newRow.find('.select2.price');
 
